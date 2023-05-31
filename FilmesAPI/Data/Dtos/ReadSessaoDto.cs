6228e6945b5e73bed553920c25523b59b51d0d10
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-05-31 09:46:58 -0300

    relacionamento entre filme e cinema + delete restrict

--- FilmesAPI/Data/Dtos/ReadSessaoDto.cs
+++ FilmesAPI/Data/Dtos/ReadSessaoDto.cs
@@ -4,7 +4,9 @@ namespace FilmesAPI.Data.Dtos
 {
     public class ReadSessaoDto
     {
-        public int Id { get; set; }
-        
+        public int FilmeId { get; set; }
+
+        public int CinemaId { get; set; }
+
     }
 }
